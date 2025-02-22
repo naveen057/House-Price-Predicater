a541485da9c4f61c6293b1c62f105a566e367e17
Thura Venkata Naveen <venkatanaveendcme057@gmail.com>
2025-02-22 15:17:57 +0530

    app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ st.subheader("Give features of the house you wish")
 
 # Input fields for user input
 bedrooms = st.number_input('No. of bedrooms in a house', min_value=1, max_value=10, value=3, step=1)
-bathrooms = st.number_input('No. of bathrooms', min_value=0.5, max_value=10.0, value=2.0, step=0.5)
+bathrooms = st.number_input('No. of bathrooms', min_value=1, max_value=10, value=2, step=1)
 sqft_living = st.slider("Select the living area (in sqft):", min_value=500, max_value=1000, step=10)
 sqft_lot = st.slider("Select the lot size (in sqft):", min_value=500, max_value=500000, value=7500, step=50)
 floors = st.number_input("Enter no. of floors", min_value=1, max_value=3, value=1)
-grade = st.slider("Select the house grade (1-13)", min_value=1, max_value=13, value=7)
+grade = st.slider("Select the house grade (1-10)", min_value=1, max_value=10, value=7)
 zipcode = st.text_input("Enter the zipcode (numeric only)")
 
 if zipcode:
